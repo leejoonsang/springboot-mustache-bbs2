4712fa9c241a6658d88fcdff16af60d69e54695c
Sangjoon <djm04212@naver.com>
2022-11-14 15:24:39 +0900

    JPQL 실습 - between, orderBy

--- src/main/java/com/mustache/bbs2/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbs2/repository/HospitalRepository.java
@@ -7,4 +7,10 @@ import java.util.List;
 
 public interface HospitalRepository extends JpaRepository<Hospital, Long> {
     List<Hospital> findByBusinessTypeNameIn(List<String> businessTypeName);
+    List<Hospital> findByRoadNameAddressContaining(String str);
+    List<Hospital> findByRoadNameAddressStartsWith(String str);
+    List<Hospital> findByRoadNameAddressEndingWith(String str);
+    List<Hospital> findByPatientRoomCountBetween(int num1, int num2);
+    List<Hospital> findByPatientRoomCountBetweenOrderByPatientRoomCount(int num1, int num2);
+
 }
--- src/test/java/com/mustache/bbs2/repository/HospitalRepositoryTest.java
+++ src/test/java/com/mustache/bbs2/repository/HospitalRepositoryTest.java
@@ -17,6 +17,16 @@ class HospitalRepositoryTest {
     HospitalRepository hospitalRepository;
 
     @Test
+    @DisplayName("출력하는 코드 분리")
+    void printHospitalNameAndAddress(List<Hospital> hospitals) {
+        for(var hospital : hospitals){
+            System.out.printf("%s | %s\n", hospital.getHospitalName(), hospital.getRoadNameAddress());
+        }
+
+        System.out.println(hospitals.size());
+    }
+
+    @Test
     @DisplayName("BusinessTypeName 보건소, 보건지소, 보건진료소가 잘 나오는지 test")
     void findByBusinessTypeNameIn() {
         List<String> inClues = new ArrayList<>();
@@ -28,4 +38,26 @@ class HospitalRepositoryTest {
             System.out.println(hospital.getHospitalName());
         }
     }
+
+    @Test
+    @DisplayName("특정 지역을 포함하는 병원이 잘 나오는지 test")
+    void containing() {
+        List<Hospital> hospitals = hospitalRepository.findByRoadNameAddressContaining("광진구");
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    @DisplayName("병상 수가 10~20개인 병원 찾기 test")
+    void between() {
+        List<Hospital> hospitals = hospitalRepository.findByPatientRoomCountBetween(10, 20);
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    @DisplayName("병상 수가 10~20개인 병원을 병상 수 오름차순으로 정렬 test")
+    void betweenAndOrderBy() {
+        List<Hospital> hospitals = hospitalRepository.findByPatientRoomCountBetweenOrderByPatientRoomCount(10, 20);
+        printHospitalNameAndAddress(hospitals);
+    }
+
 }
